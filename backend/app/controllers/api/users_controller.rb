72ababd8547f96188426ca5010cfd8c380a00829
Julie <julie.lin0812@gmail.com>
2017-12-09 20:32:41 -0800

    Create user create and show methods

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -1,17 +1,28 @@
 class Api::UsersController < ApplicationController
-  
+
   def create
-    
+    @user = User.new(user_params)
+    if @user.save!
+      login(@user)
+      render 'api/users/show'
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
   end
-  
+
   def show
-    
+    @user = User.find_by(params[:id])
+    if @user
+      render :show
+    else
+      render json: ['User not found'], status: 422
+    end
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:first_name, :last_name, :username, :password, :is_instructor)
   end
-  
+
 end
