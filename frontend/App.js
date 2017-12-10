a612733569be93c2de720349f8296ca6373f2ddc
Julie <julie.lin0812@gmail.com>
2017-12-10 02:47:17 -0800

    Commit

--- frontend/App.js
+++ frontend/App.js
@@ -4,7 +4,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import RootReducer from './src/reducers/RootReducer';
+import RootReducer from './src/reducers/root_reducer';
 import LoginForm from './src/components/LoginForm';
 
 class App extends Component {
@@ -23,8 +23,6 @@ export default App;
 
 
 
-// 
-// 
-// 
-
-
+//
+//
+//
