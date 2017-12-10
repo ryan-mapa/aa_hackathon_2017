fb7cb48dc78c9862b05c807785e759eef9b3232b
David <davidgbanks91@gmail.com>
2017-12-09 23:01:57 -0800

    Write create show index for Group and Assignment controllers

--- backend/app/controllers/api/assignments_controller.rb
+++ backend/app/controllers/api/assignments_controller.rb
@@ -1,10 +1,29 @@
 class Api::AssignmentsController < ApplicationController
   def create
+    @assignment = Assignment.new(assignment_params)
+    if @assignment.save
+      render :show
+    else
+      render json: @assignment.errors.full_messages, status: 422
+    end
   end
 
   def index
+    @assignments = Assignment.where(class_id: params[:class_id])
+    render :index
   end
 
   def show
+    @assignment = Assignment.find_by(params[:id])
+    if @assignment
+      render :show
+    else
+      render json: ['Assignment not found'], status: 422
+    end
   end
+  
+  def assignment_params
+    params.require(:assignment).permit(:name, :description, :point_value, :due_date, :class_id)
+  end
+  
 end
