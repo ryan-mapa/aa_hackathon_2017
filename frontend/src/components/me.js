e1cba4c31a95d31ee5d0fa6f3da5e25d3bf80d09
ryan mapa <ryan.m.mapa@gmail.com>
2017-12-10 14:58:34 -0800

    Set up me components

---
+++ frontend/src/components/me.js
@@ -0,0 +1,25 @@
+
+import React, { Component } from 'react';
+import { View, Text,TextInput, ActivityIndicator } from 'react-native';
+import Button from 'react-native-button';
+import _ from 'lodash';
+
+class Me extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return <View>
+
+    </View>;
+  }
+
+
+}
+
+const styles = {
+
+ };
+
+export default Me;
---
+++ frontend/src/components/me_container.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+// import { login } from '../actions/session_actions';
+
+import Me from './me';
+
+const mapStateToProps = state => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Me);
