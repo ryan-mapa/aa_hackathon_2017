c90ac907b54efe54b8044c30fddfeca560416cb8
David <davidgbanks91@gmail.com>
2017-12-10 19:00:08 -0800

    Change session[:sessionToken] to params[:sessionToken]

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -4,17 +4,17 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
   
   def current_user
-    @current_user ||= User.find_by_session_token(session[:session_token])
+    @current_user ||= User.find_by_session_token(params[:session_token])
   end
   
   def login(user)
     @current_user = user
-    session[:session_token] = @current_user.reset_session_token
+    params[:session_token] = @current_user.reset_session_token
   end
   
   def logout
     current_user.reset_session_token
-    session[:session_token] = nil
+    params[:session_token] = nil
     @current_user = nil
   end
   
